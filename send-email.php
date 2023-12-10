995b96e3ab478d1134178fc4528d2319350dd79f
Uroš Bajić <57686216+uros-bajic@users.noreply.github.com>
2023-12-10 20:58:04 +0100

    Update send-email.php

--- send-email.php
+++ send-email.php
@@ -1,5 +1,9 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
 $name = $_POST["name"];
 $email = $_POST["email"];
 $subject = $_POST["subject"];
