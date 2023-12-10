ec857dfe85363854dd86bc35dc415750d6e27e31
Uroš Bajić <57686216+uros-bajic@users.noreply.github.com>
2023-12-10 20:49:37 +0100

    Add files via upload

---
+++ send-email.php
@@ -0,0 +1,37 @@
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "exbp642@gmail.com";
+$mail->Password = "lfmffjcihyeqbzbd";
+
+$mail->setFrom($email);
+$mail->addAddress("bajicuros@live.com", "Dave");
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+header("Location: sent.html");
+
+?>
